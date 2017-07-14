d1718742088d18cfd314439d2d620936ac62669b
ex1eet <exfortune@bk.ru>
2017-07-14 19:06:37 +0300

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -1,6 +1,19 @@
 
+$(document).ready(function(){
+	          $('.slick-slider').slick({dots: true,
+		        infinite: true,
+		        slidesToShow: 3,
+		        slidesToScroll: 3});	
+	        /* $('.click-slider').slick(); */
 
-jQuery(document).ready(function($) {
-	$('.drop-menu').click(function(event) {
-		$(this).next().slideToggle();
-	});
+			        $(".slowly").on("click", function (event) {
+		          //отменяем стандартную обработку нажатия по ссылке
+		          event.preventDefault();
+		          //забираем идентификатор бока с атрибута href
+		          var id  = $(this).attr('href'),
+		          //узнаем высоту от начала страницы до блока на который ссылается якорь
+		            top = $(id).offset().top;
+		          //анимируем переход на расстояние - top за 600 мс
+		          $('body,html').animate({scrollTop: top}, 600);
+		        });
+	        });
